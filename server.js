1eb81ee699134120edcad7dddd0d369e442a30fc
Mercetti <Mercetti@users.noreply.github.com>
2026-01-09 18:32:57 -0600

    AI Control Center Optimizations

--- server.js
+++ server.js
@@ -30,6 +30,20 @@ const io = socketIo(server, {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
