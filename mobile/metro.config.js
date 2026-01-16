6a93f9e34f5da44d9aa110f13be3c8cebdf21a4f
Mercetti <Mercetti@users.noreply.github.com>
2026-01-16 15:25:00 -0600

    Acey Mobile App Beta

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -3,7 +3,7 @@
  * Bundler for the mobile app
  */
 
-const { getDefaultConfig } = require('metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 
 module.exports = (async () => {
   const defaultConfig = await getDefaultConfig(__dirname);
