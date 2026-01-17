5f136a5828bd2404437a52208429fb7f5476eb80
Mercetti <Mercetti@users.noreply.github.com>
2026-01-16 21:57:45 -0600

    Acey Mobile App Beta

---
+++ apps/mobile-web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.merce.aceycontrol',
+  appName: 'Acey Control Center',
+  webDir: 'dist'
+};
+
+export default config;
