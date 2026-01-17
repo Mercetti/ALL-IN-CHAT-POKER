eda3f0a5108352d636640f751371219796a21272
Mercetti <Mercetti@users.noreply.github.com>
2026-01-16 20:06:39 -0600

    Acey Mobile App Beta

---
+++ clean-workspace/mobile/src/screens/TestScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TestScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#030712' }}>
+      <Text style={{ color: '#ffffff', fontSize: 24 }}>
+        Acey Control Center
+      </Text>
+    </View>
+  );
+};
+
+export default TestScreen;
