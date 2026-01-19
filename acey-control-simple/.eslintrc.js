f1ed28b9301998c77dafccf4d34a82c407d94fff
Mercetti <Mercetti@users.noreply.github.com>
2026-01-18 23:40:10 -0600

    Acey Mobile App Beta

--- acey-control-simple/.eslintrc.js
+++ acey-control-simple/.eslintrc.js
@@ -1,50 +1,14 @@
+/* eslint-env node */
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
     node: true,
     jest: true,
-    'react-native/react-native': true,
   },
-  extends: [
-    'eslint:recommended',
-    '@react-native-community',
-  ],
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    requireConfigFile: false,
-  },
-  plugins: [
-    'react',
-    'react-native',
-  ],
+  extends: ['@react-native-community'],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    'no-unused-vars': 'warn',
     'no-undef': 'off',
-  },
-  globals: {
-    console: 'writable',
-    require: 'writable',
-    __dirname: 'readonly',
-    setTimeout: 'readonly',
-    alert: 'readonly',
-    Device: 'readonly',
-    jest: 'readonly',
-    beforeEach: 'readonly',
-    afterEach: 'readonly',
-    describe: 'readonly',
-    test: 'readonly',
-    expect: 'readonly',
-    it: 'readonly',
-    module: 'writable',
-    process: 'writable',
-    global: 'writable'
+    'no-unused-vars': 'warn',
   }
 };
